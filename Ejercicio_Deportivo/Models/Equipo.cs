73313b90039d1cb52d7f86d892eab5d792ef20fe
Mateos20013 <130107102+Mateos20013@users.noreply.github.com>
2024-10-17 08:42:15 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_Deportivo/Models/Equipo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ejercicio_Deportivo.Models
+{
+    public class Equipo
+    {
+        public int Id { get; set; }
+        [MaxLength(200)]
+        [Required]
+        public string Nombre { get; set; }
+        [MaxLength(200)]
+    }
+}
---
+++ Ejercicio_Deportivo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio_Deportivo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
